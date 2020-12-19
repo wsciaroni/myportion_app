3692708fb92c456b4c1ee53159309ca4d8668f93
Will Sciaroni <wsciaroni@gmail.com>
2020-12-18 22:55:17 -0600

    Create opening screen. Scren is a splash page where the user will choose create account or login.

---
+++ lib/screens/opening.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class OpeningScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+
+    final logo = Image.asset(
+      "assets/logo.png",
+      fit: BoxFit.contain,
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          print("Login Pressed");
+        },
+      ),
+    );
+
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Register",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          print("Register Pressed");
+        },
+      ),
+    );
+
+    final buttons = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        loginButton,
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 15.0, 0, 40.0),
+          child: registerButton,
+        ),
+      ],
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(36.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: logo,
+            ),
+            Text(
+              "MyPortion App",
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            buttons,
+          ],
+        ),
+      ),
+    );
+  }
+}
